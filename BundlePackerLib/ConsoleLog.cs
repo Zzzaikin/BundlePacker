3e3d6ccb814ceff681e5b66fbbfb71871b03f1fa
Zzzaikin <zaikin.pakedovo@gmail.com>
2022-08-06 17:52:43 +0300

    Add a Console logger

---
+++ BundlePackerLib/ConsoleLog.cs
@@ -0,0 +1,36 @@
+﻿namespace BundlePackerLib
+{
+    public class ConsoleLog
+    {
+        public void Success(string message)
+        {
+            ValidateMessage(message);
+            WriteColorfulMessage(message, ConsoleColor.Green);
+        }
+
+        public void Error(string message)
+        {
+            ValidateMessage(message);
+            WriteColorfulMessage(message, ConsoleColor.Red);
+        }
+
+        public void Warn(string message)
+        {
+            ValidateMessage(message);
+            WriteColorfulMessage(message, ConsoleColor.Yellow);
+        }
+
+        private void WriteColorfulMessage(string message, ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine($"{message}");
+            Console.ResetColor();
+        }
+
+        private void ValidateMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message)) 
+                throw new ArgumentException(nameof(message));
+        }
+    }
+}
